56fa275cb4ac5c1d277744f7583bfde7d6b0c8bd
Anantmanas <anantmanas303@gmail.com>
2023-07-13 21:09:39 +0530

    google and github user login & signup integrated

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,10 +1,25 @@
-import NextAuth from "next-auth/next";
+import NextAuth,{AuthOptions} from "next-auth";
 import Credentials from "next-auth/providers/credentials";
 import prismadb from "@/lib/prismadb"
 import {compare} from "bcrypt"
 
-export default NextAuth({
+import GithubProvider from "next-auth/providers/github"
+import GoogleProvider from "next-auth/providers/google"
+
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+
+export const authOptions: AuthOptions = {
     providers:[
+        GithubProvider({
+            clientId: process.env.GITHUB_ID || '',
+            clientSecret: process.env.GITHUB_SECRET || ''
+        }),
+
+        GoogleProvider({
+            clientId: process.env.GOOGLE_CLIENT_ID || '',
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET || ''
+        }),
+
         Credentials({
             id: "credentials",
             name: "Credentials",
@@ -19,13 +34,13 @@ export default NextAuth({
                 }
             },
            async authorize (credentials){
-              if (!credentials?.email || credentials?.password){
+              if (!credentials?.email || !credentials?.password){
                 throw new Error("Email and password required");
               }
 
               const user = await prismadb.user.findUnique({
                 where:{
-                    email: credentials?.email
+                    email: credentials.email
                 }
               });
               if(!user || !user.hashedPassword){
@@ -45,7 +60,8 @@ export default NextAuth({
     pages:{
         signIn:'/auth',
     },
-    debug:process.env.NODE_ENV == 'development',
+    debug: process.env.NODE_ENV == 'development',
+    adapter: PrismaAdapter(prismadb),
     session:{
         strategy:'jwt',
     },
@@ -53,4 +69,6 @@ export default NextAuth({
         secret: process.env.NEXTAUTH_JWT_SECRET,
     },
     secret: process.env.NEXTAUTH_SECRET,
-})
+}
+
+export default NextAuth(authOptions);
--- pages/auth.tsx
+++ pages/auth.tsx
@@ -1,30 +1,57 @@
 import axios from 'axios';
 import Input from "@/components/input"
 import { useCallback, useState } from "react"
-import {signIn} from 'next-auth/react'
+import { signIn } from 'next-auth/react'
+import { useRouter } from 'next/router';
+
+import {FcGoogle} from 'react-icons/fc'
+import {FaGithub} from 'react-icons/fa'
 
 const Auth = () => {
+	const router = useRouter();
+
 	const [email, setEmail] = useState('');
 	const [name, setName] = useState('');
 	const [password, setPassword] = useState('');
-	const [variant, setVariant] = useState('');
+	const [variant, setVariant] = useState('login');
 
 	const toggleVariant = useCallback(() => {
-		setVariant((currentVariant)=>currentVariant === 'login' ? 'register' : 'login');
-	},[]);
+		setVariant((currentVariant) => currentVariant === 'login' ? 'register' : 'login');
+	}, []);
 
-	const register = useCallback(async() =>{
-        try{
-     await axios.post('/api/register',{
-		email,
-		name,
-		password,
-	 })
+	const login = useCallback(async () => {
+		try {
+			await signIn('credentials', {
+				email,
+				password,
+				redirect: false,
+				callbackUrl: '/'
+			});
+
+			router.push('/')
 		}
-		catch(err){
+		catch (err) {
 			console.log(err);
 		}
-	},[email,name,password])
+	}, [email, password, router]);
+
+
+	const register = useCallback(async () => {
+		try {
+			await axios.post('/api/register', {
+				email,
+				name,
+				password,
+			});
+			
+			login();
+		}
+		catch (err) {
+			console.log(err);
+		}
+	}, [email, name, password, login]);
+
+
 
 	return (
 		<div className="relative h-full w-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-center bg-fixed bg-cover">
@@ -38,16 +65,25 @@ const Auth = () => {
 							{variant === 'login' ? 'Sign In' : 'Register'}
 						</h2>
 						<div className="flex flex-col gap-4">
-							{variant === 'register' &&(
-							<Input id="Username" label="Username" onChange={(e:any) => setName(e.target.value)} value={name} />
+							{variant === 'register' && (
+								<Input id="Username" label="Username" onChange={(e: any) => setName(e.target.value)} value={name} />
 							)}
-							<Input type="email" id="email"  label="Email" onChange={(e:any) => setEmail(e.target.value)} value={email} />
-							<Input type="password" id="password"label="Password" onChange={(e:any) => setPassword(e.target.value)} value={password}  />
+							<Input type="email" id="email" label="Email" onChange={(e: any) => setEmail(e.target.value)} value={email} />
+							<Input type="password" id="password" label="Password" onChange={(e: any) => setPassword(e.target.value)} value={password} />
 						</div>
-						<button onClick={register} className="bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 transition">{variant === 'login' ? 'Login' : 'Sign up'}</button>
+						<button onClick={variant === 'login' ? login : register} className="bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 transition">{variant === 'login' ? 'Login' : 'Sign up'}</button>
+
+						<div className='flex flex-row item-center gap-4 mt-8 justify-center'>
+							<div onClick={()=> signIn('google',{callbackUrl:'/'})} className='w-10 h-10 bg-white rounded-full flex items-center justify-center cursor-pointer hover:opacity-80 transition'>
+								<FcGoogle size={30}/>
+							</div>
+							<div onClick={()=> signIn('github',{callbackUrl:'/'})} className='w-10 h-10 bg-white rounded-full flex items-center justify-center cursor-pointer hover:opacity-80 transition'>
+								<FaGithub size={30}/>
+							</div>
+						</div>
 						<p className="text-neutral-500 mt-12">
 							{variant === 'login' ? 'First time using Netflix?' : 'Already have an account?'}
-							<span onClick={toggleVariant} className="text-white ml-1 hover:underline cursor-pointer">{variant === 'login' ? 'Create an account' :'Login here'}</span>
+							<span onClick={toggleVariant} className="text-white ml-1 hover:underline cursor-pointer">{variant === 'login' ? 'Create an account' : 'Login here'}</span>
 						</p>
 					</div>
 				</div>
